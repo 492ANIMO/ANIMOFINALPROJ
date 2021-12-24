aa976334540e06f9a359a0bd581b95b4818b8467
Nipaporn Meetubtim <myungmay42@gmail.com>
2021-12-24 21:40:51 +0700

    delete comment

--- backend/routes/annoucement.js
+++ backend/routes/annoucement.js
@@ -6,11 +6,8 @@ const annoucementController = require('../controllers/annoucementController.js')
 /* GET users listing. */
 router.get('/', annoucementController.index);
 router.get('/:id', annoucementController.show);
-// router.get('/pet/:petId', annoucementController.showByPet);
-// router.get('/client/:clientId', annoucementController.showByOwner);
 router.post('/', annoucementController.create);
 router.delete('/:id', annoucementController.destroy);
-// // router.delete('/', annoucementController.destroyAll);
 router.patch('/:id', annoucementController.update);
 
 
