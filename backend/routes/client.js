a6be241497c135273707e348680b3bafaabbc9c2
Nipaporn Meetubtim <myungmay42@gmail.com>
2022-01-16 16:05:03 +0700

    add passport jwt authentication

--- backend/routes/client.js
+++ backend/routes/client.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const passportJWT = require('../middleware/passportJWT');
+
 const clientController = require('../controllers/clientController');
 const upload = require('../middleware/upload');
 /* GET users listing. */
-router.get('/', clientController.index);
+router.get('/',  clientController.index);
 router.get('/:id', clientController.show);
 router.post('/',upload.single('avatar') , clientController.create);
 router.delete('/:id', clientController.destroy);
