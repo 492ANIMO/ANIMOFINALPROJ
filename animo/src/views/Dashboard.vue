b25945ccdce3c33cfe42e5d32e0f2b321714a13c
Nipaporn Meetubtim <myungmay42@gmail.com>
2022-01-16 17:21:14 +0700

    chart

--- animo/src/views/Dashboard.vue
+++ animo/src/views/Dashboard.vue
@@ -18,7 +18,7 @@
 <script>
 import Navbar from "@/components/Navbar.vue";
 import NavbarSide from "@/components/NavbarSide.vue";
-import { config } from "vue/types/umd";
+
 
 export default {
   name: "Dashboard",
@@ -26,42 +26,53 @@ export default {
     Navbar,
     NavbarSide,
   },
+  data: () => ({
+  }),
+  created() {
+    this.chart();
+  },
+
+  methods: {
+    chart(){
+        const labels = ["January", "February", "March", "April", "May", "June"];
+
+        const data = {
+          labels: labels,
+          datasets: [
+            {
+              label: "My First dataset",
+              backgroundColor: "rgb(255, 99, 132)",
+              borderColor: "rgb(255, 99, 132)",
+              data: [0, 10, 5, 2, 20, 30, 45],
+            },
+          ],
+        };
+
+        const config = {
+          type: "line",
+          data: data,
+          options: {},
+        };
+        const myChart = new Chart(
+          document.getElementById('myChart'),
+          config
+        );
+    }
+  }
 };
 
-const labels = ["January", "February", "March", "April", "May", "June"];
+// const labels = ["January", "February", "March", "April", "May", "June"];
 
-// console.log('888==================',ctx);
-//  document.onload=function(){
-//    const ctx=document.getElementById('myChart');
-//     console.log('==================',ctx);
-//    };
+// // console.log('888==================',ctx);
+// //  document.onload=function(){
+// //    const ctx=document.getElementById('myChart');
+// //     console.log('==================',ctx);
+// //    };
 
-window.onload = function () {
-  const labels = ["January", "February", "March", "April", "May", "June"];
+// window.onload = function () {
 
-  const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: "My First dataset",
-        backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "rgb(255, 99, 132)",
-        data: [0, 10, 5, 2, 20, 30, 45],
-      },
-    ],
-  };
-
-  const config = {
-    type: "line",
-    data: data,
-    options: {},
-  };
-   const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
   
-};
+// };
 
 
 // function loaded(){
