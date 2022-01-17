a0e751694639a1c23cccd5115614e397d5ad84d7
Nipaporn Meetubtim <myungmay42@gmail.com>
2022-01-18 02:21:14 +0700

    refactor

--- backend/routes/annoucement.js
+++ backend/routes/annoucement.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const annoucementController = require('../controllers/annoucementController.js');
-
+// middleware
+const passportJWT = require('../middleware/passportJWT');
+const passportLocal = require('../middleware/checkAuth');
+const checkRole = require('../middleware/checkRole');
 const upload = require('../middleware/upload');
 
 /* GET users listing. */
+// router.get('/', [passportLocal.isLogin], annoucementController.index);
 router.get('/', annoucementController.index);
 router.get('/:id', annoucementController.show);
 router.post('/', upload.single('img'), annoucementController.create);
